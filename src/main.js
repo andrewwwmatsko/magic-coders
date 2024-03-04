efa93ba3ceecbfdb418ba06ade2dfc43e6bea7f9
andrewwwmatsko <matskoandrew@gmail.com>
2024-03-04 18:24:29 +0200

    added smooth scroll JS

--- src/main.js
+++ src/main.js
@@ -14,3 +14,15 @@
     refs.modal.classList.toggle('is-open');
   }
 })();
+
+// Smooth scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});

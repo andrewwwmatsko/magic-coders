8b968fe637ef1c33fbde2e5fdd742c888aaf0552
andrewwwmatsko <matskoandrew@gmail.com>
2024-03-07 15:43:55 +0200

    improved JS

--- src/main.js
+++ src/main.js
@@ -17,7 +17,11 @@
 
 //
 
-jQuery(document).on('click', '.mob-menu-item', function () {
+jQuery(document).on('click', '.mob-menu-link', function () {
+  jQuery('.mob-menu-item').closest('.backdrop').removeClass('is-open');
+});
+
+jQuery(document).on('click', '.button-shop', function () {
   jQuery('.mob-menu-item').closest('.backdrop').removeClass('is-open');
 });
 
@@ -27,11 +31,19 @@ jQuery(document).on('click', '.mob-menu-item', function () {
   const refs = {
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
+    howItWorks: document.querySelector('[data-how-it-works]'),
+    vegetables: document.querySelector('[data-vegetables]'),
+    reviews: document.querySelector('[data-reviews]'),
+    shopNowBtn: document.querySelector('[data-shopNowBtn]'),
     body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleStopScroll);
   refs.closeModalBtn.addEventListener('click', toggleStopScroll);
+  refs.howItWorks.addEventListener('click', toggleStopScroll);
+  refs.vegetables.addEventListener('click', toggleStopScroll);
+  refs.reviews.addEventListener('click', toggleStopScroll);
+  refs.shopNowBtn.addEventListener('click', toggleStopScroll);
 
   function toggleStopScroll() {
     refs.body.classList.toggle('fixed-position');
